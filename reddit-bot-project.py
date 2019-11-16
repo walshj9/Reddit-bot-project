5aaf56b31bc25b57e9d4d1249d9924c8210b89db
walshj9 <walshj9@students.rowan.edu>
2019-11-16 16:35:15 -0500

    Separated into different methods

--- reddit-bot-project.py
+++ reddit-bot-project.py
@@ -1,29 +1,33 @@
-import requests
+import os
 import praw
 import sys
 
 with open('../reddit-bot-data.txt') as f:
     info = f.read().splitlines()
-print(info)
+
 
 uname = info[0]
 pw = info[1]
 acc_id = info[2]
 acc_secret = info[3]
+evlis_passw = info[4]
 agent = 'school_project-bot by /u/school_project-bot'
 
-redditauth= praw.Reddit(client_id = acc_id,
-                     client_secret = acc_secret,
-                     password = pw,
-                     user_agent= agent,
-                     username= uname)
+#logs into reddit
+def reddit_login():
+    
+    redditauth= praw.Reddit(client_id = acc_id,
+                        client_secret = acc_secret,
+                        password = pw,
+                        user_agent= agent,
+                        username= uname)
+    return redditauth.user.me()
+#gets suthorization link
+def get_auth_link():
+    auth_link = praw.Reddit(client_id= acc_id,
+                        client_secret = acc_secret,
+                        redirect_uri = 'http://localhost:8080',
+                        user_agent = agent )
+    return auth_link.auth.url(['identity'], '...', 'permanent')
 
-reddit = praw.Reddit(client_id= acc_id,
-                     client_secret = acc_secret,
-                     redirect_uri = 'http://localhost:8080',
-                     user_agent = agent )
-
-print(redditauth.user.me())
-print("\n")
-print(reddit.auth.url(['identity'], '...', 'permanent'))
 print("\nPass")
